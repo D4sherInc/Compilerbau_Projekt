3132918a2e1a1ef621171652b8372ed2e1ba0deb
D4sherInc <phil.klotzsche@gmail.com>
2021-11-28 19:52:46 +0100

    StupsCompiler.java: added Tokenizer + print
    sablecc.scc: more tokens, all_chars newline is invalid (?)

--- src/main/java/StupsCompiler.java
+++ src/main/java/StupsCompiler.java
@@ -1,5 +1,33 @@
-public class StupsCompiler {
-    private double x = 200;
-    private double y = 100;
+import lexer.Lexer;
+import lexer.LexerException;
+import node.EOF;
+import node.Token;
 
+import java.io.IOException;
+import java.io.PushbackReader;
+import java.io.StringReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StupsCompiler {
+    public static void main(String[] args) throws IOException, LexerException {
+        Path path = Paths.get("./test1.cs");
+        List<String> input = Files.lines(path).collect(Collectors.toList());
+        int lineCounter = 1;
+        for (String line : input) {
+            StringReader reader = new StringReader(line);
+            PushbackReader r = new PushbackReader(reader);
+            Lexer l = new Lexer(r);
+            Token token;
+            System.out.println("LINE " + lineCounter);
+            do {
+                token = l.next();
+                System.out.println(token.getClass().getSimpleName() + " " + token.getText());
+            } while (!(token instanceof EOF));
+            lineCounter++;
+        }
+    }
 }
