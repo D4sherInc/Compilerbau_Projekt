b309bb0fcf09eccbbf5965dc11ed45a99cb9fb6d
D4sherInc <phil.klotzsche@gmail.com>
2021-12-12 20:27:40 +0100

    StupsCompiler.java: lexical analysis (lex) and syntactical analysis (parse) extracted into methods, try-catch for errors

--- src/main/java/StupsCompiler.java
+++ src/main/java/StupsCompiler.java
@@ -1,43 +1,45 @@
 import lexer.Lexer;
 import lexer.LexerException;
 import node.EOF;
+import node.Start;
 import node.TWhitespace;
 import node.Token;
+import parser.Parser;
+import parser.ParserException;
 
-import java.io.IOException;
-import java.io.PushbackReader;
-import java.io.StringReader;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.LinkedList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class StupsCompiler {
-    public static void main(String[] args) throws IOException, LexerException {
-        // java StupsCompiler
-        // java StupsCompiler ../resources/minimal.cs
+    public static void main(String[] args) throws IOException, LexerException, ParserException {
+        // java StupsCompiler <filepath>
+        // java StupsCompiler minimal.cs
         Path path_to_file = Paths.get(args[0]);
-        lex(path_to_file);
-
-    }
-
-    private static void lex(Path path) throws IOException {
-
-        List<String> input;
+        List<String> inputAsList;
 
         // try-catch for IOException: wrong filepath
         try{
-            input = Files.lines(path).collect(Collectors.toList());
+            inputAsList = Files.lines(path_to_file).collect(Collectors.toList());
         }
         catch (IOException e) {
             System.err.println("ERROR: no such file found, try another path");
-            input = new LinkedList<>();
+            return;
         }
 
-        int lineCounter = 1;
+        String input = new Scanner(new File(String.valueOf(path_to_file))).useDelimiter("\\Z").next();
+
+        boolean success = lex(inputAsList);
+        if (success) parse(input);
+    }
+
+    private static boolean lex(List<String> input) throws IOException {
         boolean lexErrorFound = false;
+        int lineCounter = 1;
         for (String line : input) {
             StringReader reader = new StringReader(line);
             PushbackReader r = new PushbackReader(reader);
@@ -45,13 +47,14 @@ public class StupsCompiler {
             Token token;
             do {
 
-                // try-catch for lexical error: token not found --> print "unknown token: *token*"
+                // try-catch for lexical error: token not found --> print "unknown token: 't'"
                 // add Whitespace token (gets ignored), --> keep going
                 try{
                     token = l.next();
                 }
                 catch (LexerException e) {
-                    System.out.println("ERROR ON LINE " + lineCounter + " " + e.getMessage());
+                    System.err.println("ERROR ON LINE " + lineCounter + ":" + e.getMessage().substring(e.getMessage().indexOf("]") + 1));
+                    System.err.println(e.getMessage());
                     token = new TWhitespace(" ");
                     lexErrorFound = true;
                 }
@@ -60,5 +63,26 @@ public class StupsCompiler {
         }
 
         if (!lexErrorFound) System.out.println("lexing successful");
+        return !lexErrorFound;
     }
+
+    private static void parse(String input) throws IOException, ParserException, LexerException {
+            StringReader reader = new StringReader(input);
+            PushbackReader r = new PushbackReader(reader, 100);
+            Lexer l = new Lexer(r);
+            Parser parser = new Parser(l);
+
+            boolean parseErrorFound = false;
+
+            try{
+                Start start = parser.parse();
+            }
+            catch (ParserException e) {
+                System.err.println("LINE " + e.getToken().getLine() + ": found '" + e.getToken().getText() + "', expected: " + e.getMessage().substring(e.getMessage().indexOf('\'')));
+                System.err.println(e.getMessage());
+                parseErrorFound = true;
+            }
+        if (!parseErrorFound) System.out.println("parsing successful");
+    }
+
 }
