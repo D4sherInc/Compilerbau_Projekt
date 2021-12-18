6fe5d994d3fd8705172591ed980114e6fb07b5f7
D4sherInc <phil.klotzsche@gmail.com>
2021-12-18 01:41:42 +0100

    added Freischuss files

---
+++ src/main/java/LineLexer.java
@@ -0,0 +1,43 @@
+import lexer.Lexer;
+import lexer.LexerException;
+import node.EOF;
+import node.TWhitespace;
+import node.Token;
+
+import java.io.IOException;
+import java.io.PushbackReader;
+import java.io.StringReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LineLexer {
+    public static void main(String[] args) throws IOException, LexerException {
+        Path path = Paths.get("minimal.cs");
+        List<String> input = Files.lines(path).collect(Collectors.toList());
+        int lineCounter = 1;
+        for (String line : input) {
+            StringReader reader = new StringReader(line);
+            PushbackReader r = new PushbackReader(reader);
+            Lexer l = new Lexer(r);
+            Token token;
+            System.out.println("\nLINE " + lineCounter);
+            do {
+
+                // try-catch for lexical error: token not found --> print "unknown token: *token*"
+                // add Whitespace token (gets ignored), --> keep going
+                try{
+                    token = l.next();
+                }
+                catch (LexerException e) {
+                    System.out.println("LINE " + lineCounter + " " + e.getMessage());
+                    token = new TWhitespace(" ");
+                }
+                if (!token.getClass().getSimpleName().equals("TWhitespace")) System.out.println(token.getClass().getSimpleName() + " " + token.getText());
+            } while (!(token instanceof EOF));
+            lineCounter++;
+        }
+    }
+}
---
+++ src/main/java/MainProgramPrinter.java
@@ -0,0 +1,39 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.PushbackReader;
+import java.io.StringReader;
+import java.util.Scanner;
+
+import parser.Parser;
+import parser.ParserException;
+
+import lexer.Lexer;
+import lexer.LexerException;
+import node.Start;
+
+public class MainProgramPrinter {
+
+
+	public static void main(String[] args) throws
+			LexerException, IOException, ParserException {
+
+		String input = new Scanner(new File("minimal.cs")).useDelimiter("\\Z").next();
+		parse(input);
+
+		//Sally false *(true + false)
+		//Sally 33 ok+7 ok *(5 ok +1 ok )
+		//New intro1.start(name.name1,expr.expra) 33 ok+7 ok *(5 ok +1 ok )
+	}
+
+	private static void parse(String input) throws ParserException,
+			LexerException, IOException {
+		StringReader reader = new StringReader(input);
+		PushbackReader r = new PushbackReader(reader, 100);
+		Lexer l = new Lexer(r);
+		Parser parser = new Parser(l);
+		Start start = parser.parse();
+		ASTPrinter printer = new ASTPrinter();
+		start.apply(printer);
+	}
+
+}
