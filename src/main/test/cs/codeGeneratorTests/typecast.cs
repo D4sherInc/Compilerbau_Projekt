db03bac3369da865d50f5605c1f816d3d9ee67f7
D4sherInc <phil.klotzsche@gmail.com>
2022-01-30 00:57:07 +0100

    typecast.cs: testfile added

---
+++ src/main/test/cs/codeGeneratorTests/typecast.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 2.0;
+            double d = 3;
+        }
+    }
+}
