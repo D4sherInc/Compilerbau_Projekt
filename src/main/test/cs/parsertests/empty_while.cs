cf1a9d279922fcb42f176745d163f1848c726ded
D4sherInc <phil.klotzsche@gmail.com>
2022-01-30 21:57:33 +0100

    testcase added

---
+++ src/main/test/cs/parsertests/empty_while.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while ()
+            {
+            
+            }
+
+        }
+    }
+}
