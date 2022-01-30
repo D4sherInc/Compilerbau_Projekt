6a4a71f038b5b72f20503e625f710e5b21849513
D4sherInc <phil.klotzsche@gmail.com>
2022-01-30 15:43:47 +0100

    Fibonacci recursive call and print

--- src/main/test/cs/codeGeneratorTests/fibonacci.cs
+++ src/main/test/cs/codeGeneratorTests/fibonacci.cs
@@ -8,6 +8,7 @@ namespace HelloWorld
         {
             Console.WriteLine(Fib(5));
             Console.WriteLine(Fib(10));
+            int x = FibRec(15);
         }
 
         static int Fib(int n)
@@ -21,6 +22,17 @@ namespace HelloWorld
             } else {
             return 0;
             }
+        }
+
+        static int FibRec(int n)
+        {
+            int i = 0;
+            while (i < n)
+            {
+                Console.WriteLine(Fib(i));
+                i = i + 1;
+            }
+            return 0;
         }
     }
 }
